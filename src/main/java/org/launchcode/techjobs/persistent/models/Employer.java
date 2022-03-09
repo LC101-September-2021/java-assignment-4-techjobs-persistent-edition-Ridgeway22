a427cf29c466312c44cba71ff7de326190f6cb75
Ridgeway22 <boo22mickey@yahoo.com>
2022-03-09 16:54:05 -0600

    git commit -m "Data layer added"

--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,6 +1,27 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.Valid;
+
+
+//For the purposes of this application, an employer can only have one location.
+@Entity
 public class Employer extends AbstractEntity {
+    @Valid
+    private String location;
 
+    public Employer(String location) {
+        this.location = location;
+    }
 
+    public Employer(){
+
+    }
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
--- src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Skill.java
@@ -1,5 +1,27 @@
 package org.launchcode.techjobs.persistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @Size(min=3, message="Name must be at least 3 characters long")
+    private String description;
+
+    public Skill(String description) {
+        this.description = description;
+    }
+
+    public Skill() {
+
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
