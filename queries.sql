2694914e94e03e8adeea2e4ece893a19f2d4ae45
Ridgeway22 <boo22mickey@yahoo.com>
2022-03-10 20:14:53 -0600

    git commit -m "fixed comment issue "

--- queries.sql
+++ queries.sql
@@ -7,3 +7,8 @@ WHERE location = "St. Louis City";
 -- Part 3: Test it with SQL
 DROP TABLE job;
 -- Part 4: Test it with SQL
+SELECT *
+FROM skills
+LEFT INNER JOIN job_skills ON(skill.id = job_skills.skills.id )
+WHERE job_skills.job_Id IS NOT NULL
+ORDER BY name ASC;
