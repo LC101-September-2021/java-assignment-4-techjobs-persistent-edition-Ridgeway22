bb7f38b17a5099c5d286ad82187521623afab60e
Ridgeway22 <boo22mickey@yahoo.com>
2022-03-12 09:45:09 -0600

    git commit -m "sql 1 fixed"

--- queries.sql
+++ queries.sql
@@ -1,6 +1,5 @@
 -- Part 1: Test it with SQL
-SELECT *
-FROM job;
+id (int), name varchar(250), employer_id (int);
 -- Part 2: Test it with SQL
 SELECT name
 FROM employer
@@ -10,6 +9,6 @@ DROP TABLE job;
 -- Part 4: Test it with SQL
 SELECT *
 FROM skill
-INNER JOIN job_skills ON (skill.id = job_skills.skills_id)
+LEFT JOIN job_skills ON skill.id = job_skills.skills_id
 WHERE job_skills.jobs_Id IS NOT NULL
 ORDER BY name ASC;
